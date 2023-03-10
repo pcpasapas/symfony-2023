5dbd63a3ab9c18e40ce4a3ad5a2ee32629372154
pcpasapas <pcpasapas@gmail.com>
2023-03-10 09:11:43 +0100

    https

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import path from 'path'
 // import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	server: {
+		https: true,
+	},
 	resolve: {
 		alias: {
 			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
